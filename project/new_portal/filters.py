7a7f0a06d4f082e939a30b9bd1066593dca92de8
vibe-34 <vibe.34@gmail.com>
2025-01-15 23:55:41 +0300

    Скорректирован class PostFilter, добавлен выбор времени через календарь и выбор автора публикации

--- project/new_portal/filters.py
+++ project/new_portal/filters.py
@@ -1,19 +1,24 @@
 from django import forms
-from django_filters import FilterSet, CharFilter, DateFilter
-from .models import Post
+from django_filters import FilterSet, CharFilter, DateFilter, ModelChoiceFilter
+from .models import Post, Author
 
 
 class PostFilter(FilterSet):
-    author = CharFilter(field_name='author__user__username', label='Author', lookup_expr='iexact')
-    title = CharFilter(field_name='title', label='Title', lookup_expr='iregex')  # icontains не работает без учета регистра именно в SQLite
-    # time_in = DateFilter(field_name='time_in', widget=forms.TextInput(attrs={'placeholder': 'YYYY-MM-DD'}),
-    #                      label='Date of publication', lookup_expr='gt')
+    author = ModelChoiceFilter(queryset=Author.objects.all(),
+                               field_name='author__user__username',
+                               label='Author',
+                               empty_label="Выберите автора",
+                               lookup_expr='iexact')
+
+    title = CharFilter(field_name='title',
+                       label='Title',
+                       lookup_expr='iregex')  # icontains не работает без учета регистра именно в SQLite
+
     time_in = DateFilter(
-        field_name='time_in',
-        widget=forms.DateInput(attrs={'type': 'date'}),
-        label='Дата публикации',
-        lookup_expr='date__gte',
-    )
+                        field_name='time_in',
+                        widget=forms.DateInput(attrs={'type': 'date'}),
+                        label='Дата публикации',
+                        lookup_expr='date__gte',)
 
     class Meta:
         model = Post
